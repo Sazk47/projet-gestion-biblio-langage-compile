a60901d1615ce593279e1b2fba5315933f54b093
hjonatha <jonathan.hasard@ynov.com>
2026-06-18 12:19:28 +0200

    [FIX] fixing,ui.h

--- include/ui.h
+++ include/ui.h
@@ -1,10 +1,38 @@
-#ifndef UI_H
-#define UI_H
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-void ui_afficher_menu(void);
+#include "ui.h"
 
-int ui_lire_entier(const char *invite);
+void ui_afficher_menu(void)
+{
+    system("clear");
+    printf("=== BIBLIOTHEQUE ===\n");
+    printf("1. Ajouter un livre\n");
+    printf("2. Afficher tous les livres\n");
+    printf("3. Rechercher\n");
+    printf("4. Emprunter / Rendre\n");
+    printf("5. Supprimer un livre\n");
+    printf("6. Sauvegarder et quitter\n");
+}
 
-void ui_lire_chaine(const char *invite, char *dest, int taille);
+int ui_lire_entier(const char *invite)
+{
+    int valeur = 0;
+    printf("%s", invite);
+    if (scanf("%d", &valeur) != 1) {
+        valeur = -1;
+    }
+    while (getchar() != '\n' && !feof(stdin)) { /* vide le buffer */ }
+    return valeur;
+}
 
-#endif
+void ui_lire_chaine(const char *invite, char *dest, int taille)
+{
+    printf("%s", invite);
+    if (fgets(dest, taille, stdin)) {
+        dest[strcspn(dest, "\n")] = '\0';
+    } else {
+        dest[0] = '\0';
+    }
+}
